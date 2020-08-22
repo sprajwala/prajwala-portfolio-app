82c7ab88db802161cb57de8049ad06f709269d0c
Prajwala Srivatsa <prajwala955@gmail.com>
2020-08-22 07:25:42 -0400

    Updates

---
+++ precache-manifest.f21e0c4948699bae2deedd4bd3d693e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10f0b96389990ededa450f20ea19474",
+    "url": "/prajwala-portfolio-app/index.html"
+  },
+  {
+    "revision": "f2189c3c4e598941a1df",
+    "url": "/prajwala-portfolio-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e45228d97278ff41368",
+    "url": "/prajwala-portfolio-app/static/js/2.f960d3f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prajwala-portfolio-app/static/js/2.f960d3f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2189c3c4e598941a1df",
+    "url": "/prajwala-portfolio-app/static/js/main.2b2fb1ed.chunk.js"
+  },
+  {
+    "revision": "f583e4a2de53b474e965",
+    "url": "/prajwala-portfolio-app/static/js/runtime-main.394d31c0.js"
+  }
+]);
